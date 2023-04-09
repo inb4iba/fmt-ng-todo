8dbe3d6aad38ef1d3be376d342f7d26ab15eb36f
Icaro <iba1987@gmail.com>
2023-04-09 01:46:43 -0300

    main features complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from "@angular/core";
 export class AppComponent {
   tasks: string[] = [];
   taskInput = "";
+
+  addTask() {
+    if (this.taskInput) this.tasks.push(this.taskInput);
+    this.taskInput = "";
+  }
+
+  updateStyle(event: any) {
+    if (event.target.checked) event.target.nextSibling.classList = "lined";
+    else event.target.nextSibling.classList = "";
+  }
 }
